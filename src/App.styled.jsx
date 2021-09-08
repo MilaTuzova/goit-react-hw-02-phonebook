0bda5fce26543de3fc61c37b097614b0eb8fc686
MilaTuzova <tuzovals@gmail.com>
2021-09-09 01:06:37 +0300

    added files

---
+++ src/App.styled.jsx
@@ -0,0 +1,7 @@
+import styled from '@emotion/styled';
+
+export const ContainerContacts = styled.div`
+  display: inline-block;
+  padding: 20px 10px;
+  margin: 20px 20px;
+`;
---
+++ src/Components/ContactForm/ContactForm.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { FormContact, Label, Input, Btn } from './ContactForm.styled';
+
+class ContactForm extends Component {
+  render() {
+    return (
+      <>
+        <FormContact>
+          <Label>
+            Name
+            <Input
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+              required
+            />
+          </Label>
+
+          <Label>
+            Number
+            <Input
+              type="tel"
+              name="number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+              required
+            />
+          </Label>
+          <Btn type="submit">Add contact</Btn>
+        </FormContact>
+      </>
+    );
+  }
+}
+
+export default ContactForm;
